d2fc3ea66a44e740ba4ee77f9f71832597aace7c
Teerasej <teerasej@gmail.com>
2021-08-12 12:20:46 +0700

    คืนข้อมูลข่าว 3 ข่าวในรูปแบบ json

--- app.js
+++ app.js
@@ -6,13 +6,17 @@ app.get('/', function (req, res) {
 })
 
 app.get('/news', function(request, response) {
-    
+    response.json([
+        { id: 0, content: 'abc' },
+        { id: 1, content: 'def' },
+        { id: 2, content: 'ghi' }
+    ])
 })
 
 app.post('/news/create', function(request, response) {
-    
+
 })
 
 app.listen(3000, function(){
-    console.log('server is running...')
+    console.log('server is running port 3000...')
 })
